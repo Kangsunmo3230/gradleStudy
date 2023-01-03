d0cf297b75e2eaa4d972a2d2da89490cdd39d4c1
sunmo <ksm0212000@naver.com>
2023-01-03 20:52:36 +0900

    -230103 -gradle 세팅 완료22

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyApplication {
-
+   //222222
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
     }
